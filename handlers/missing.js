f90e6a653d705df88afbe21f60ce1eb807cf2b97
LiCern <lisacernilogar@me.com>
2020-08-07 11:46:57 +0100

    Add missing handler
    Fixes #26

---
+++ handlers/missing.js
@@ -0,0 +1,6 @@
+function missing(request, response) {
+    response.writeHead(404, { "content-type": "text/html" });
+    response.end("<h1>Not found</h1>");
+  }
+  
+  module.exports = missing;
--- router.js
+++ router.js
@@ -3,25 +3,25 @@ const formHandler = require("./handlers/form");
 const homeHandler = require("./handlers/home");
 const publicHandler = require("./handlers/public");
 const submitHandler = require("./handlers/submit");
-const addPostHandler = require("./handlers/getposts")
+const addPostHandler = require("./handlers/getposts");
+const missingHandler = require("./handlers/missing");
 
 function router(request, response) {
  
     const url = request.url;
     const method = request.method;
-    if (error) {
-        response.writeHead(404, { "content-type": "text/html" });
-        response.end("<h1>Not Found</h1>");
-    } else if (url === '/') {
+    if (url === '/') {
         homeHandler(request,response);
     } else if (url.includes('public')) {
-        publicHandler(request, response)
+        publicHandler(request, response);
     } else if (url === '/form') {
-        formHandler(request, response)
+        formHandler(request, response);
     } else if (method === "POST" && url === "/submit") {
         submitHandler(request, response);
     } else if (method === "GET" && url === "/submit") { 
-    addPostHandler(request, response);  
+        addPostHandler(request, response);  
+    } else {
+        missingHandler(request, response);
   }   
 }
 module.exports = router;
